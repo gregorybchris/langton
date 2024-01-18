b32be005f35d591bf801f1ace52790df9abcd26d
Chris Gregory <gregorybchris@gmail.com>
2024-01-17 19:46:50 -0800

    Update colors

--- langton/cli/cli.py
+++ langton/cli/cli.py
@@ -28,21 +28,30 @@ def main() -> None:
 @click.argument("filepath", type=Path)
 @click.option("--iter", "iterations", type=int, default=1000)
 @click.option("--fps", type=float, default=40.0)
+@click.option("--width", type=int, default=100)
+@click.option("--height", type=int, default=100)
 @click.option("--debug", type=bool, is_flag=True)
 def run(
     filepath: Path,
     iterations: int,
     fps: float,
+    width: int,
+    height: int,
     debug: bool = False,
 ) -> None:
     set_logging_config(debug)
 
-    width = 140
-    height = 140
-
     with Graphics.context(filepath=filepath, width=width, height=height, fps=fps) as graphics:
         rules = [
-            Rule(current_color=Color.BLACK, turn=True, new_color=Color.WHITE),
+            Rule(current_color=Color.BLACK, turn=False, new_color=Color.RED),
+            Rule(current_color=Color.RED, turn=False, new_color=Color.BLUE),
+            Rule(current_color=Color.BLUE, turn=False, new_color=Color.YELLOW),
+            Rule(current_color=Color.YELLOW, turn=True, new_color=Color.GREEN),
+            Rule(current_color=Color.GREEN, turn=False, new_color=Color.YELLOW),
+            Rule(current_color=Color.YELLOW, turn=True, new_color=Color.VIOLET),
+            Rule(current_color=Color.VIOLET, turn=True, new_color=Color.TEAL),
+            Rule(current_color=Color.TEAL, turn=False, new_color=Color.ORANGE),
+            Rule(current_color=Color.ORANGE, turn=True, new_color=Color.WHITE),
             Rule(current_color=Color.WHITE, turn=False, new_color=Color.BLACK),
         ]
         policy = Policy(rules=rules)
--- langton/lib/color.py
+++ langton/lib/color.py
@@ -7,19 +7,25 @@ class Color(StrEnum):
     GREEN = auto()
     BLUE = auto()
     YELLOW = auto()
-    CYAN = auto()
-    MAGENTA = auto()
+    TEAL = auto()
+    VIOLET = auto()
+    ORANGE = auto()
+    NAVY = auto()
+    PINK = auto()
     WHITE = auto()
     BLACK = auto()
 
     def to_rgb(self) -> Tuple[int, int, int]:
         return {
-            Color.RED: (255, 0, 0),
-            Color.GREEN: (0, 255, 0),
-            Color.BLUE: (0, 0, 255),
-            Color.YELLOW: (255, 255, 0),
-            Color.CYAN: (0, 255, 255),
-            Color.MAGENTA: (255, 0, 255),
-            Color.WHITE: (255, 255, 255),
+            Color.RED: (231, 76, 60),
+            Color.GREEN: (46, 204, 113),
+            Color.BLUE: (52, 152, 219),
+            Color.YELLOW: (241, 196, 15),
+            Color.TEAL: (26, 188, 156),
+            Color.VIOLET: (155, 89, 182),
+            Color.ORANGE: (230, 126, 34),
+            Color.NAVY: (52, 73, 94),
+            Color.PINK: (243, 104, 224),
+            Color.WHITE: (236, 240, 241),
             Color.BLACK: (0, 0, 0),
         }[self]
